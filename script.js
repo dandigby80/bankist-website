b12c5bb59551fc93b3c1b7b0e8700106ed24934c
Dan Digby <dandigby@me.com>
2021-04-16 21:49:51 +0100

    Added tabbed component and menu fade animation

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -37,7 +41,7 @@ document.addEventListener('keydown', function (e) {
 //Scrolling 
 btnScrollTo.addEventListener('click', function(e){
   const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords)
+  
   // window.scrollTo(s1coords.left, s1coords.top + window.pageYOffset);
 
   window.scrollTo({
@@ -73,5 +77,41 @@ document.querySelector('.nav__links').addEventListener('click', function(e){
   }
   console.log(e.target);
 })
+
+// Tabbed Component
+
+
+tabsContainer.addEventListener('click', function(e){
+  const clicked = e.target.closest('.operations__tab');
+  //guard clause
+  if(!clicked) return;
+  //active tab
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'))
+  clicked.classList.add('operations__tab--active');
+  //activate content area
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+ console.log(clicked.dataset.tab)
+})
+
+//Menu fade animation
+
+const handleHover = function(e){
+  console.log(this)
+  if(e.target.classList.contains('nav__link')){
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+  
+  siblings.forEach(el => {
+    if(el !== link) el.style.opacity = this;
+  });
+  logo.style.opacity = this;
+  }
+}
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleHover.bind(1));
 
 
