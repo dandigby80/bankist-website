188a9c216a12d2ea489e9f652cbf6f66300551ad
Dan Digby <dandigby@me.com>
2021-04-17 17:45:59 +0100

    Nav scroll event

--- script.js
+++ script.js
@@ -114,4 +114,16 @@ nav.addEventListener('mouseover', handleHover.bind(0.5));
 
 nav.addEventListener('mouseout', handleHover.bind(1));
 
+//Sticky Navigation
+const initialCoords = section1.getBoundingClientRect();
+console.log(initialCoords)
+
+window.addEventListener('scroll', function(){
+  if(window.scrollY > initialCoords.top){
+    nav.classList.add('sticky');
+  }else {
+    nav.classList.remove('sticky');
+  }
+})
+
 
