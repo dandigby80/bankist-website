befd413f096bf866b852b835cb3fcf2d9ac5cbc2
Dan Digby <dandigby@me.com>
2021-04-18 18:02:40 +0100

    Revealing sections on scroll

--- script.js
+++ script.js
@@ -97,7 +97,6 @@ tabsContainer.addEventListener('click', function(e){
 //Menu fade animation
 
 const handleHover = function(e){
-  console.log(this)
   if(e.target.classList.contains('nav__link')){
     const link = e.target;
     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
@@ -141,10 +140,9 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 
 const header = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect().height;
-console.log(navHeight.height)
+
 const stickyNav = function(entries){
   const [entry] = entries;
-  console.log(entry)
   if(!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
 }
@@ -155,6 +153,29 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 });
 
 headerObserver.observe(header);
+
+// Reveal sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function(entries, observer){
+  const [entry] = entries;
+  console.log(entry)
+  if(!entry.isIntersecting) return
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+}
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function(section){
+  sectionObserver.observe(section)
+  section.classList.add('section--hidden');
+})
+
+
 
 
 
